00f71d7227fbee2a0e0165ef0ba5b20b62a8e768
nikitha <nikitha@LAPTOP-4GAKUODQ>
2022-02-04 11:51:53 +0530

    first commit

---
+++ 04-02-2022/src/com/javaDataTypes/Example1.java
@@ -0,0 +1,16 @@
+package com.javaDataTypes;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		 int customerId = 1234;
+		String customerName = "laddu";
+		String customerAddress = "Hyderabad";
+		
+		System.out.println(customerId);
+		System.out.println(customerName);
+		System.out.println(customerAddress);
+	}
+
+}
